be1fb58c57862b59671af81ef53c1909900ef2a9
Landon Taylor <56940292+landonjefftaylor@users.noreply.github.com>
2022-02-28 17:23:55 -0700

    Update prism_api.py

--- prism_api.py
+++ prism_api.py
@@ -7,6 +7,14 @@ def get_intersection(trace):
     last_enabled = []
     curr_enabled = []
 
+    # Clean up after the binary file
+    # TODO make a better binary file
+    trace_a = trace.split("\n")
+    for line in trace_a:
+        if "bin/run" in line or "length" in line:
+            del line
+    trace = '\n'.join(trace_a)
+
     # if "TARGET" not in trace:
     #     print("ERROR: DOES NOT APPEAR TO REACH TARGET.")
     #     print("ERROR THROWN BY prism_api.py")
@@ -21,8 +29,6 @@ def get_intersection(trace):
         if "pathProbability " in line:
             print("path probability found: " + line.split("pathProbability ")[1])
             break
-        if "bin/run" in line or "length" in line:
-            continue
         last_enabled = curr_enabled
         curr_enabled = []
         # print("curr_enabled = " + str(curr_enabled))
