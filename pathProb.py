11248b821f4beafb2e59f0a0064f0bef91339e10
Landon Taylor <56940292+landonjefftaylor@users.noreply.github.com>
2022-03-02 13:52:08 -0700

    Create pathProb.py

---
+++ pathProb.py
@@ -0,0 +1,15 @@
+
+class pathProb():
+  def __init__(self):
+    self.prob = 1.0
+
+  def accumulate(self, x):
+    self.prob = self.prob * x
+
+  def readProbabilityFromString(self, s):
+    if "pathProbability " not in s:
+      print("ERROR: pathProbability not in input file.")
+    else:
+      newProb = float(s.split("pathProbability ").strip()[1])
+      self.prob = self.prob * newProb
+      print("newProb", newProb, "self.prob", self.prob)
