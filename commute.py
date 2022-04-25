d0085a3a215b51a1aa5a045033e84a42c741e68b
Landon Taylor <56940292+landonjefftaylor@users.noreply.github.com>
2022-04-25 15:51:49 -0600

    BIG UPDATE to COMMUTE

--- commute.py
+++ commute.py
@@ -5,29 +5,32 @@ import prism_api
 import commute
 import pathProb
 import os
+import tempfile
 
+
+# Merged into commutePath to help with some logistics
 # Commute the discovered transitions (intersection) throughout the trace.
-def commute(trace, intersection):
-  with open("commuted.trace", 'w') as commuted:
-    # Get individual moves
-    t = trace.replace("\t\t", "\t").rstrip().lstrip().split("\t")
-    # Remove any empty spots in t
-    while "" in t:
-      t.remove("")
-    # Loop through every discovered transition in the intersection
-    for i in intersection:
-      # Loop through the whole array
-      for a in range(0,len(t)):
-        # Get the commuted transition's "prefix"
-        for b in range(0,a):
-          commuted.write(t[b] + "\t")
-        # Write the commuted transition
-        commuted.write(i + "\t")
-        # Get the commuted transition's "suffix"
-        for c in range(a,len(t)):
-          commuted.write(t[c] + "\t")
-        # Reset for next trace
-        commuted.write("\n")
+# def commute(trace, intersection):
+#   with open("commuted.trace", 'w') as commuted:
+#     # Get individual moves
+#     t = trace.replace("\t\t", "\t").rstrip().lstrip().split("\t")
+#     # Remove any empty spots in t
+#     while "" in t:
+#       t.remove("")
+#     # Loop through every discovered transition in the intersection
+#     for i in intersection:
+#       # Loop through the whole array
+#       for a in range(0,len(t)):
+#         # Get the commuted transition's "prefix"
+#         for b in range(0,a):
+#           commuted.write(t[b] + "\t")
+#         # Write the commuted transition
+#         commuted.write(i + "\t")
+#         # Get the commuted transition's "suffix"
+#         for c in range(a,len(t)):
+#           commuted.write(t[c] + "\t")
+#         # Reset for next trace
+#         commuted.write("\n")
 
 
 # Take in a string path from IVy, commute, and give back a total probability.
@@ -35,29 +38,10 @@ def commutePath(ivy_path, api_result, pathP):
   print("commutePath initialized.")
   print(">>", ivy_path.replace("\t"," "))
 
-  #### THE FOLLOWING WAS MOVED INTO ITS OWN FUNCTION CALLED BEFORE COMMUTEPATH
-  # # Save the path to forprism.trace (name mandatory)
-  # with open("forprism.trace", 'w') as trace:
-  #   trace.write(ivy_path)
-
-  # # Have the PRISM API walk along the path, reporting enabled transitions
-  # try:
-  #   # recompile the java in case of updates
-  #   os.system("make test > prism.result")
-  #   # using depreciated os.system because subprocess was not working
-  #   # result = subprocess.check_output(['make','test'])
-  # except:
-  #   print("os.system Error!")
-  #   quit()
-
-  # # api_result = result.read()
-  # with open("prism.result") as result:
-  #   api_result = result.read()
-  ###########################################################################
+  temp_result = tempfile.NamedTemporaryFile(mode="w+")
 
   # add in the probability
-  # TODO: Does prism api read in the probabilities or the rates with getTransitionProbability???
-  print("Prism Returns the first time:")
+  print("Prism Returns the first time during this function call:")
   pathP.readProbabilityFromString(api_result)
 
   # Find the intersection of all enabled transitions
@@ -74,9 +58,6 @@ def commutePath(ivy_path, api_result, pathP):
     print("Single path probability:", pathP.prob)
     return intersection
     # quit()
-    
-  # Build paths with the enabled transitions commuted
-  commute(ivy_path, intersection)
   
   # Find out if t_alpha will get you to a target state
   # Maybe just do this in the Java script... whatever's more efficient
@@ -85,40 +66,74 @@ def commutePath(ivy_path, api_result, pathP):
     with open("forprism.trace", "w") as temp_path:
       temp_path.write(newpath)
     try:
-      os.system("make test > prism.result")
+      os.system("make test > " + temp_result.name)
+      # os.system("make test > prism.result")
     except:
       print("os.system Error in commuted trace!")
       continue
-    with open("prism.result") as result:
-      if "ERR_TAR_NO_RCH" in result.read():
-        print("Commuting", t_alpha, "does not lead to a target state.")
-        intersection.remove(t_alpha)
-        print("Removed", t_alpha, "from inspection.")
-      else:
-        print("Commuting", t_alpha, "leads to a target state :)")
 
+    temp_result.seek(0)
+
+    if "ERR_TAR_NO_RCH" in temp_result.read():
+      print("Commuting", t_alpha, "does not lead to a target state.")
+      intersection.remove(t_alpha)
+      print("Removed", t_alpha, "from inspection.")
+    else:
+      print("Commuting", t_alpha, "leads to a target state :)")
+
+  # Build paths with the enabled transitions commuted
+  # commute(ivy_path, intersection)
+
+  commuted_paths = tempfile.NamedTemporaryFile(mode="w+")
+
+  # Get individual moves
+  t = ivy_path.replace("\t\t", "\t").rstrip().lstrip().split("\t")
+  # Remove any empty spots in t
+  while "" in t:
+    t.remove("")
+  # Loop through every discovered transition in the intersection
+  for i in intersection:
+    # Loop through the whole array
+    for a in range(0,len(t)):
+      # Get the commuted transition's "prefix"
+      for b in range(0,a):
+        commuted_paths.write(t[b] + "\t")
+      # Write the commuted transition
+      commuted_paths.write(i + "\t")
+      # Get the commuted transition's "suffix"
+      for c in range(a,len(t)):
+        commuted_paths.write(t[c] + "\t")
+      # Reset for next trace
+      commuted_paths.write("\n")
+
+  commuted_paths.seek(0)
 
   # Go through the traces, accumulate probability
   # This may be better to do in Java actually...
-  with open("commuted.trace") as commuted:
-    for trace in commuted:
-      with open("forprism.trace", "w") as forprism:
-        # Isolate one trace at a time to send to the api
-        forprism.write(trace.rstrip())
-      try:
-        # os.system("make") # may be needed, idk why though
-        os.system("make test > prism.result")
-      except:
-        print("os.system Error in commuted trace!")
-        continue
-      # Read the results back in
-      # TODO eventually add some amount of recursion here to 
-      # commute in the new traces
-      with open("prism.result") as result:
-        api_result = result.read()
-      # add in the probability
-      print("Prism Returns the second time:")
-      print("trace >>", trace)
-      pathP.readProbabilityFromString(api_result)
+  # with open("commuted.trace") as commuted:
+  for commuted_path in commuted_paths:
+    with open("forprism.trace", "w") as forprism:
+      # Isolate one trace at a time to send to the api
+      forprism.write(commuted_path.rstrip())
+    try:
+      # os.system("make") # may be needed, idk why though
+      os.system("make test > " + temp_result.name)
+    except:
+      print("os.system Error in commuted trace!")
+      continue
+    # Read the results back in
+    # TODO eventually add some amount of recursion here to 
+    # commute in the new traces
+    temp_result.seek(0)
+    # api_result = temp_result.read()
+
+    api_result = prism_api.getEnabledTransitions(commuted_path)
+    nested_intersection = commute.commutePath(commuted_path, api_result, pathP)
+
+    # add in the probability -- removed due to recursive function call instead
+    # print("Prism Returns the second time:")
+    # print("trace >>", trace)
+    # pathP.readProbabilityFromString(api_result)
+
 
   return intersection
