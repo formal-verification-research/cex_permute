131277c83c829feba64d7ac9ee0ef1291173237f
Landon Taylor <56940292+landonjefftaylor@users.noreply.github.com>
2022-05-16 14:25:00 -0600

    Update SimulateModel.java

--- src/SimulateModel.java
+++ src/SimulateModel.java
@@ -204,7 +204,7 @@ public class SimulateModel
         // Print the states along the original path
         System.out.println("States along Original Path");
         for (int i = 0; i < rollingStateIndex-1; i++) {
-          System.out.println(String.format("%2d: [%d %d %d %d %d %d]", i, state.get(i)[0], state.get(i)[1], state.get(i)[2], state.get(i)[3], state.get(i)[4], state.get(i)[5]));
+          System.out.println(String.format("%2d: [%d %d %d %d %d %d]", i, states.get(i)[0], states.get(i)[1], states.get(i)[2], states.get(i)[3], states.get(i)[4], states.get(i)[5]));
         }
         System.out.println("Original Path Complete.");
         
