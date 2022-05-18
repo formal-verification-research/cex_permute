55fcbda580c91aadc7f71d1ab355d565cae4e407
Landon Taylor <56940292+landonjefftaylor@users.noreply.github.com>
2022-05-18 10:36:10 -0600

    Update SimulateModel.java

--- src/SimulateModel.java
+++ src/SimulateModel.java
@@ -6,6 +6,9 @@ import java.io.FileNotFoundException;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 import java.io.IOException;
 
 import java.util.ArrayList;
