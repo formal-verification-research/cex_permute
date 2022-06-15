731baa423bfe0980a937e7d3f0c93f2e19781734
Landon Taylor <56940292+landonjefftaylor@users.noreply.github.com>
2022-06-15 10:38:03 -0600

    Update BuildModel.java

--- src/BuildModel.java
+++ src/BuildModel.java
@@ -639,8 +639,8 @@ public class BuildModel
         String finalTransition = "";
 
         // Find and fire the commuted transition
-        System.out.println("01. Checking " + String.format("[%s]", path.commutable.get(t_alpha)));
         String pathTransition = String.format("[%s]", path.commutable.get(t_alpha));
+        System.out.println("01. Checking " + pathTransition);
         for (int i=0; i < sim.getNumTransitions(); i++) {
           // Get transition strings from path and simulation
           String simTransition = sim.getTransitionActionString(i);
@@ -700,9 +700,10 @@ public class BuildModel
         sim.backtrackTo(simStateIndex);
 
         // Find the original path transition
+        pathTransition = String.format("[%s]", transitions.get(t));
+        System.out.println("02. Checking " + pathTransition);
         for (int i=0; i < sim.getNumTransitions(); i++) {
           // Get transition strings from path and simulation
-          String pathTransition = String.format("[%s]", transitions.get(t));
           String simTransition = sim.getTransitionActionString(i);
           // Update index if we found the right transition
           if (pathTransition.equalsIgnoreCase(simTransition)) {
