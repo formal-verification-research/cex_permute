8d7bc1ee25107017082a0e7fe033a1dbc349849e
Landon Taylor <landon.jeffrey.taylor@usu.edu>
2022-11-08 16:15:31 -0700

    Improved Uniqueness

--- depthChecker.py
+++ depthChecker.py
@@ -8,7 +8,7 @@ for i in range(-1,25):
             print("on phase " + str(i))
             new.write(oldStr.replace("public static final int MAX_DEPTH = 0;", "public static final int MAX_DEPTH = " + str(i) + ";"))
             os.system("make > /dev/null 2>&1")
-            fldr = "timed_recdepth/" + str(i)
+            fldr = "better_dupe/" + str(i)
             os.system("mkdir " + fldr + " > /dev/null 2>&1")
             os.system("/usr/bin/time -o" + fldr + "/time.txt make test > " + fldr + "/result.txt")
             os.system("mv prism.lab prism.sta prism.tra storm.lab storm.tra " + fldr)
