fa0f1aff32524315f484874675a68b02568f6953
bryantisraelsen <bryant.israelsen@gmail.com>
2023-01-28 20:09:47 +0000

    plots added from depth testing

--- plotProbvsRecursion.py
+++ plotProbvsRecursion.py
@@ -3,15 +3,6 @@ import math
 import os
 import random
 import sys
-import matplotlib.pyplot as plt
-#from matplotlib import rc, rcParams
-from matplotlib.backends.backend_pdf import PdfPages
-#import numpy as np
-
-plt.rcParams["font.family"] = "Arial"
-plt.rcParams["font.size"] = "18"
-plt.rcParams["mathtext.fontset"] = "cm"
-plt.rcParams["axes.labelweight"] = "bold"
 
 react = "2react"
 recursion = "1"
@@ -20,7 +11,10 @@ rootdir = '/cygdrive/c/Users/adminstrator/cex_permute/results/2reactDepth'
 
 depth = []
 prob = []
-
+time = []
+states = []
+tran = []
+count1 = 0
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
         if file == "prismOut.txt":
@@ -36,41 +30,45 @@ for subdir, dirs, files in os.walk(rootdir):
                         #print(float(line.split() [1]))
                         prob.append(float(line.split() [1]))
                     #if line[0] == "N":
+        elif file == "time.txt":
+            with open(subdir + "/time.txt", "r") as f:
+                count = 0
+                while True:
+                    count += 1
+                    line = f.readline()
+                    if not line:
+                        break
+                    if count == 1:
+                        #print(float(line[9:13]))
+                        time.append(float(line[9:13]))
+
+        elif file == "out.txt":
+            count1 += 1
+            with open(subdir + "/out.txt", "r") as f:
+                #count = 0
+                while True:
+                    #count += 1
+                    line = f.readline()
+                    #print(line.split() [1])
+                    if not line:
+                        break
+                    if len(line.split()) > 1:
+                        if (line.split() [1]) == "states":
+                            #print(int(line[0:1]))
+                            states.append(int(line.split()[0]))
+                        elif (line.split() [1]) == "transitions":
+                            #print(int(line[0:1]))
+                            tran.append(int(line.split()[0]))
+                    #if line[0] == "N":
             #print(subdir[72:75])
 
+#print(count1)
+print(time)
 print(depth)
-print(prob)
+print(tran)
+print(states)
+#print(prob)
 
-
-plt.plot(depth, prob, "o", color='teal', linewidth=3.5)
-plt.plot(depth, prob, "", color='teal', linewidth=2.5)
-
-#plt.title("Concurrent Path Builder Preliminary Result")
-
-plt.ylabel('Path Probability', fontsize=18)
-plt.yscale("log")
-
-# Setting the interval of ticks of y-axis to 10.
-#listOf_Yticks = np.arange(1E-100, 1E-20, 1E10)
-listOf_Yticks = [1e-100,1e-90,1e-80,1e-70,1e-60,1e-50,1e-40,1e-30,1e-20]
-plt.yticks(listOf_Yticks)
-
-listOf_Xticks = [4,8,12,16]
-plt.xticks(listOf_Xticks)
-
-plt.xlabel('Recursion depth', fontsize=18)
-
-plt.grid(color='gainsboro', axis='y')
-
-plt.tight_layout()
-
-plt.savefig('plot.png')
-
-with PdfPages(r'plot.pdf') as export_pdf:
-    export_pdf.savefig()
-
-plt.show()
-        
 
 
 
